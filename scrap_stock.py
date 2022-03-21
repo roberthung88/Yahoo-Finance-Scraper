4ce08579c498c44a31ac46d28cd893e79d618af9
Jui Po Hung <juipohun@usc.edu>
2022-03-21 19:00:16 -0400

    Minor Change

--- scrap_stock.py
+++ scrap_stock.py
@@ -2,8 +2,12 @@ import pandas as pd
 import yfinance as yf
 
 def main():
+    print(pd.__version__)
     input_str = input("Enter stock tick symbols: ")
     thread = input("Use Multithreading for Mass Downloads? Enter 1 (Yes) or 0 (No): ")
+    if input_str == "":
+        print("No stock symbols entered! No result will return.")
+        return
     # checking for valid input
     while(True):
         try:
@@ -18,6 +22,10 @@ def main():
     print()
     print("Checking for Symbol Validity... ")
     todays_data =  yf.download(input_str, period="1d", threads=thread)
+    # check if all input ticks are invalid
+    if todays_data.empty:
+        print("All input ticks are invalid!")
+        return
     todays_data = todays_data.dropna(axis=1)
     todays_data = todays_data.drop(['Adj Close', 'Volume'], axis=1)
     todays_data.columns = todays_data.columns.swaplevel(0, 1)
